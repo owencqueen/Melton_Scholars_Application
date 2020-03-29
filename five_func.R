9f7a530773dcabb5fd06b47b988e0123f9c8b607
owencqueen <owencqueen@hotmail.com>
2020-03-29 19:42:51 -0400

    Added comments to five_func

--- five_func.R
+++ five_func.R
@@ -1,3 +1,4 @@
+# Gets flights five airports out from ORD (or selected airport)
 five_out <- function(date, df, ret_df){
 
   if (date == 0){
@@ -12,6 +13,7 @@ five_out <- function(date, df, ret_df){
   
   ret_df = ret_df[ret_df$FL_DATE %in% date_range,]
   
+  # Begin recursive call
   new_df = five_out_rec(date_range, df, ret_df, 1)
   
   new_df = new_df %>% distinct(FL_DATE, OP_UNIQUE_CARRIER, ORIGIN_AIRPORT_ID, DEST_AIRPORT_ID, CRS_DEP_TIME, DEP_TIME, DEP_DELAY_NEW, DEP_DEL15, CRS_ARR_TIME, ARR_TIME, ARR_DELAY_NEW, ARR_DEL15, CANCELLED, WEATHER_DELAY, .keep_all = TRUE)
@@ -19,6 +21,7 @@ five_out <- function(date, df, ret_df){
   return (new_df)
 }
 
+# Recursive procedure for five_out
 five_out_rec <- function(date_range, df, ret_df, depth){
   
   # Base case: we've gone through this recursion 5 times
@@ -26,10 +29,12 @@ five_out_rec <- function(date_range, df, ret_df, depth){
     return (NULL)  
   }
   
+  # Performing BFS on whole data frame
   new_df = df[(df$FL_DATE %in% date_range) & (df$ORIGIN_AIRPORT_ID %in% ret_df$DEST_AIRPORT_ID), ]
   new_df = cbind(new_df, DEPTH = rep(depth, nrow(new_df))) 
   #   Assign how far we are from Chicago
   
+  # Recursive procedure
   extracted = five_out_rec(date_range, df, new_df, depth + 1)
   
   if (is.null(extracted)) {
@@ -41,6 +46,7 @@ five_out_rec <- function(date_range, df, ret_df, depth){
   return (ret_df)
 }
 
+# Gets date range around given date
 dr <- function(date){
   # 3 dates - 1 before, 1 after, the date itself
   
@@ -55,6 +61,7 @@ dr <- function(date){
 }
 
 # For use for INCOMING flights, not outgoing
+# Very similar to five_out
 five_in <- function(date, df, ret_df){
   
   if (date == 0){
